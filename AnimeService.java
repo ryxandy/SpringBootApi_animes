5688513335aa4725cca406d53a659e75de37ef7e
Alexandre Lopes <33386705+ryxandy@users.noreply.github.com>
2020-12-22 21:39:51 -0300

    updating service
    im adding a method where i can add some item to the array list of animes

---
+++ AnimeService.java
@@ -0,0 +1,39 @@
+package learning.springboot.springboot2essentials.service;
+
+import learning.springboot.springboot2essentials.domain.Anime;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+public class AnimeService {
+
+   private static List<Anime> animes;
+
+   static {
+      animes = new ArrayList<>(List.of(new Anime(1L,"Naruto"),new Anime(2L,"Boruto"), new Anime(3L,"Shingeki No Kyojin"),new
+               Anime(4L,"Hunter x Hunter")));
+   }
+
+
+    public List<Anime> ListAll(){
+        return animes;
+    }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(4,10000));
+        animes.add(anime);
+        return anime;
+   }
+}
