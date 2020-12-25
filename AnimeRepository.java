ff6015711d5aee221dde7bf771534dea95fdbb1a
Alexandre Lopes <33386705+ryxandy@users.noreply.github.com>
2020-12-25 11:24:03 -0300

    addint the delete method

---
+++ AnimeRepository.java
@@ -0,0 +1,12 @@
+package learning.springboot.springboot2essentials.repository;
+
+
+import learning.springboot.springboot2essentials.domain.Anime;
+
+import java.util.List;
+
+public interface AnimeRepository {
+
+
+
+}
